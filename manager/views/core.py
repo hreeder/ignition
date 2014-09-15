9ff27e8e8ac5da7f67320d79e794a0772d397243
Harry Reeder <harry@harryreeder.co.uk>
2014-09-15 22:27:44 +0100

    Add the login template & GET view

--- manager/views/core.py
+++ manager/views/core.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    regurn render_template("register.html")
