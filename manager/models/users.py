54d4d6bbba8c5e2536804ca543f1492333bb0788
Harry Reeder <harry@harryreeder.co.uk>
2014-10-13 10:54:24 +0100

    Set up flask-security

--- manager/models/users.py
+++ manager/models/users.py
@@ -1,13 +1,26 @@
 from manager import db
+from flask.ext.security import UserMixin, RoleMixin
 
-class User(db.Model):
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(256))
-    enabled = db.Column(db.Boolean)
+    active = db.Column(db.Boolean)
     superuser = db.Column(db.Boolean)
     openid_enabled = db.Column(db.Boolean)
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
     def is_authenticated():
         return True
